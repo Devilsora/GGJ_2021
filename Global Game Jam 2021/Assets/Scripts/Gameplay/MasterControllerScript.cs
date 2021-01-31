4734b0de7bbad424454ded3edd03eaa6dc52bd0a
lbc <lbcorney@yahoo.com>
2021-01-31 00:04:44 -0800

    Attempting to manage 3 Scenes

---
+++ Global Game Jam 2021/Assets/Scripts/Gameplay/MasterControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// ref: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadScene.html
+
+public class StartHereClass : MonoBehaviour
+{
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        // SceneManager.LoadScene("Scene3", LoadSceneMode.Additive);
+        Debug.Log("Start here...");
+    }
+
+     void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.N))
+      {
+        SceneManager.LoadScene("Scene3", LoadSceneMode.Additive);
+        Debug.Log("Update, N key pushed...");
+      }
+    }
+
+
+}
